81764a1fb59708401b08ef2785f4d8e5b07277fb
Airbus A330-200 <>
2021-05-22 13:27:20 +0000

    Use regex instead of HTTP requests

--- src/Functions/validation.js
+++ src/Functions/validation.js
@@ -1,37 +1,9 @@
-let fetch = require("node-fetch");
-
-exports.bot = async (token) => {
-  let f = await fetch("https://discord.com/api/gateway/bot", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": "Bot " + client.token
-    }
-  });
-  let status = await f.status;
-  if (status == 401) { 
-    return false;
-  } else if (status == 200) { 
+const validate = async (token) => {
+  if (/(mfa\.[a-z0-9_-]{20,})|([a-z0-9_-]{23,28}\.[a-z0-9_-]{6,7}\.[a-z0-9_-]{27})/i.test(token)) {
     return true;
-  } else { 
+  } else {
     return false;
   }
 };
 
-exports.user = async (token) => {
-  let f = await fetch("https://discord.com/api/gateway/bot", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": client.token
-    }
-  });
-  let status = await f.status;
-  if (status == 401) { 
-    return false;
-  } else if (status == 200) { 
-    return true;
-  } else { 
-    return false;
-  }
-};
+module.exports = { validate };
